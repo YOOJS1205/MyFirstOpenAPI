cee11c67a5cfb1dc6f3fa602e8d61ae0b57a2017
yoojs1205 <junsang8921@gmail.com>
2022-03-09 15:56:43 +0900

    3/9 : Using OPEN API

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  useEffect(async() => {
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'elfLFMKSsi0Pu6q1m%2BGTESkE3tGmwmPuR8J6jp4IL6%2Fvu7ODA6VgJ8yZgLqFUEl2M16NeZZKQnit9qB%2FPlpw%2Fw%3D%3D';
+    queryParams += '&' + encodeURIComponent('page') + '=' + encodeURI('1');
+    queryParams += '&' + encodeURIComponent('perPage') + '=' + encodeURI('20');
+
+    var url = 'https://api.odcloud.kr/api/15041653/v1/uddi:223e0a0f-f812-423a-88ce-b742300ca156' + queryParams;
+
+    try{
+      const response = await axios({
+        method: 'get',
+        url: url
+      })
+      console.log(response.data.data);
+    }
+    catch(err) {
+      console.log(err);
+    }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>멍멍이에게 멋진 이름 선물하기</h1>
+      <form>
+        <input type='text' placeholder='멋진 이름을 지어보세요...!' />
+        <button>시작!</button>
+      </form>
     </div>
   );
 }
