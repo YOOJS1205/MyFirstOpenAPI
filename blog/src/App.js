5b18939397b87dc7cf38e8026cd9636e086afeb7
yoojs1205 <junsang8921@gmail.com>
2022-03-12 17:36:49 +0900

    3/12 : 디자인 수정 + 기능 추가
    로그인하면 로그인 버튼이 로그아웃으로 바뀌는 기능 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'elfLFMKSsi0Pu6q1m%2BGTESkE3tGmwmPuR8J6jp4IL6%2Fvu7ODA6VgJ8yZgLqFUEl2M16NeZZKQnit9qB%2FPlpw%2Fw%3D%3D';
     queryParams += '&' + encodeURIComponent('page') + '=' + encodeURI('1');
-    queryParams += '&' + encodeURIComponent('perPage') + '=' + encodeURI('30');
+    queryParams += '&' + encodeURIComponent('perPage') + '=' + encodeURI('200');
     queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURI('json');
 
     var url = 'https://api.odcloud.kr/api/15041653/v1/uddi:223e0a0f-f812-423a-88ce-b742300ca156' + queryParams;
@@ -35,9 +35,9 @@ function App() {
     <div className="App">
       <div className='login'>
         <div className={userInfo.isLogin ? 'greeting' : 'greeting hidden'}>{userInfo.id}님 반갑습니다!</div>
-        <div className='login_button'><Link to='/login'>로그인</Link></div>
+        <div className='login_button'><Link to='/login'>{userInfo.isLogin ? '로그아웃' : '로그인'}</Link></div>
       </div>
-      <h1>멍멍이에게 멋진 이름 선물하기</h1>
+      <h1>댕댕이에게 멋진 이름 선물하기</h1>
       <form>
         <input type='text' placeholder='멋진 이름을 지어보세요...!'
                onChange={onChangeSearchTerm} />
